e71431c5b44e40494ba5df847800647485b0d450
David Chua <david.cys@live.com>
2015-03-21 13:18:16 +0800

    install highcharts

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ gem 'jbuilder', '~> 1.2'
 gem 'httparty'
 gem 'httmultiparty'
 gem 'foundation-rails'
+gem 'highcharts-rails'
 
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require highcharts
+//= require highcharts/highcharts-more
 //= require foundation
 //= require_tree .
 $(function(){ $(document).foundation(); });
